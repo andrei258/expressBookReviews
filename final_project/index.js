7c6da57b6bac5a4421851afd8faedfaf8cbf06a7
andrei258 <andreibalan258@gmail.com>
2024-11-22 05:09:12 -0500

    auth

--- final_project/index.js
+++ final_project/index.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
+const secretKey = 'super-secret-key';
 
 const app = express();
 
@@ -11,7 +12,18 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+    const token = req.headers['authorization'];
+    if (!token) {
+        return res.status(401).json({ message: "Access token missing" });
+    }
+
+    jwt.verify(token, secretKey, (err, decoded) => {
+        if (err) {
+            return res.status(403).json({ message: "Invalid or expired access token" });
+        }
+        req.user = decoded;
+        next();
+    });
 });
  
 const PORT =5000;
